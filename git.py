87c87ecf3118cf5937bcff8276536699e11309c3
ccf <catarina.cruz.ferreira@bancobpi.pt>
2023-01-21 12:37:58 +0000

    test

---
+++ git.py
@@ -0,0 +1,38 @@
+import shlex
+import argparse 
+import subprocess
+
+def bash(command):
+    subprocess.run(['bash', '-c', command])
+
+def git_init():
+    bash('git init')
+def git_add():
+    bash('git add --all')
+
+def git_commit(args):
+    bash('git commit -m' + f'{args.commit}')
+
+def git_branch(args):
+    bash('git branch -M' + f'{args.branch}')
+
+def git_remote():
+    bash('git remote add origin https://github.com/catarinaferrei/projeto-ito.git')
+
+def git_push(args):
+    bash('git push -u origin' + f'{args.branch}')
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--commit', help='Commit message',required=True)
+    parser.add_argument('--branch', help='Name of the branch',required=False)
+    args = parser.parse_args()
+    git_init()
+    git_add()
+    git_commit(args)
+    git_branch(args)
+    git_remote()
+    git_push(args)
+
+if __name__ == '__main__':
+    main()
